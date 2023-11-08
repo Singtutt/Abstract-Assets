5dfb7da870e9b0d0e0583188d0a241b880377a48
Sing Tuttanon <singtuttanon@gmail.com>
2023-11-08 12:01:05 -0500

    Initial Commit: Completed Asset/House/Vehicle Classes

---
+++ src/main/java/com/pluralsight/House.java
@@ -0,0 +1,61 @@
+package com.pluralsight;
+
+public class House extends Asset {
+    private String address;
+    private int condition;
+    private int squareFoot;
+    private int lotSize;
+
+    public House(String description, String dateAcquired, double originalCost, String address, int condition, int squareFoot, int lotSize) {
+        super(description, dateAcquired, originalCost);
+        this.address = address;
+        this.condition = condition;
+        this.squareFoot = squareFoot;
+        this.lotSize = lotSize;
+    }
+
+//    Getters
+    public String getAddress() {
+        return address;
+    }
+    public int getCondition() {
+        return condition;
+    }
+    public int getSquareFoot() {
+        return squareFoot;
+    }
+    public int getLotSize() {
+        return lotSize;
+    }
+
+//    Setters
+    public void setAddress(String address) {
+        this.address = address;
+    }
+    public void setCondition(int condition) {
+        this.condition = condition;
+    }
+    public void setSquareFoot(int squareFoot) {
+        this.squareFoot = squareFoot;
+    }
+    public void setLotSize(int lotSize) {
+        this.lotSize = lotSize;
+    }
+
+    @Override
+    public double getValue() {
+        double value;
+
+        if ( condition == 1) {
+            value = squareFoot * 180.00;
+        } else if (condition == 2) {
+            value = squareFoot * 130.00;
+        } else if (condition == 3) {
+            value = squareFoot * 90.00;
+        } else {
+            value = squareFoot * 80.00;
+        }
+        value += 0.25 * lotSize;
+        return value;
+    }
+}
---
+++ src/main/java/com/pluralsight/Vehicle.java
@@ -0,0 +1,59 @@
+package com.pluralsight;
+import java.time.*;
+
+public class Vehicle extends Asset {
+    private String makeModel;
+    private int year;
+    private int odometer;
+
+    public Vehicle(String description, String dateAcquired, double originalCost, String makeModel, int year, int odometer) {
+        super(description, dateAcquired, originalCost);
+        this.makeModel = makeModel;
+        this.year = year;
+        this.odometer = odometer;
+    }
+
+//    Getters
+    public String getMakeModel() {
+        return makeModel;
+    }
+    public int getYear() {
+        return year;
+    }
+    public int getOdometer() {
+        return odometer;
+    }
+
+//    Setters
+    public void setMakeModel(String makeModel) {
+        this.makeModel = makeModel;
+    }
+    public void setYear(int year) {
+        this.year = year;
+    }
+    public void setOdometer(int odometer) {
+        this.odometer = odometer;
+    }
+
+    @Override
+    public double getValue() {
+        double value;
+        int currentYear = LocalDate.now().getYear();
+        int age = currentYear - year;
+
+        if (age >= 0 && age <= 3) {
+            value = getOriginalCost() * (1 - (0.03 * age));
+        } else if (age >= 4 && age <= 6) {
+            value = getOriginalCost() * (1 - (0.06 * age));
+        } else if (age >= 7 && age <= 10) {
+            value = getOriginalCost() * (1 - (0.08 * age));
+        } else {
+            value = getOriginalCost() * 1000.00;
+        }
+
+        if (odometer > 100000 && !makeModel.toLowerCase().contains("honda") && !makeModel.toLowerCase().contains("toyota")) {
+            value *= 0.75;
+        }
+        return value;
+    }
+}
